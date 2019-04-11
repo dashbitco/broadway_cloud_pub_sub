97dd5ba1ece209169adae3a499385d7bc4c24ef2
Michael Crumm <mike@crumm.net>
2019-04-11 10:56:26 -0700

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule BroadwayCloudPubSub.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "A Google Cloud Pub/Sub connector for Broadway"
   @repo_url "https://github.com/mcrumm/broadway_cloud_pub_sub"
 
