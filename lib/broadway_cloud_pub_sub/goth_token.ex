09a9cab7f54e61fd81cf888c616cdb1bc2c25585
Michael Crumm <mike@crumm.net>
2019-08-16 13:50:28 -0700

    Deprecate BroadwayCloudPubSub.Token behaviour

--- lib/broadway_cloud_pub_sub/goth_token.ex
+++ lib/broadway_cloud_pub_sub/goth_token.ex
@@ -1,11 +1,6 @@
 if Code.ensure_compiled?(Goth) do
   defmodule BroadwayCloudPubSub.GothToken do
-    @moduledoc """
-    Default token module used by `BroadwayCloudPubSub.Producer` to authenticate with Google.
-
-    Authentication is handled by the `Goth` library, which must be explicitly included
-    in your dependencies and configured for your application.
-    """
+    @moduledoc false
 
     @behaviour BroadwayCloudPubSub.Token
 
