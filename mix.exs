342ee82914166d7ab7bfe4cc5092b3c0424878ac
Wojtek Mach <wojtek@wojtekmach.pl>
2022-05-09 16:16:49 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule BroadwayCloudPubSub.MixProject do
   use Mix.Project
 
-  @version "0.7.1"
+  @version "0.8.0-dev"
   @description "A Google Cloud Pub/Sub connector for Broadway"
   @repo_url "https://github.com/dashbitco/broadway_cloud_pub_sub"
 
