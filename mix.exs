6546bdca9f9223f0d32f76002c8bb6d35f7e8c8b
Wojtek Mach <wojtek@wojtekmach.pl>
2022-05-09 16:12:55 +0200

    Release v0.7.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule BroadwayCloudPubSub.MixProject do
   use Mix.Project
 
-  @version "0.7.0"
+  @version "0.7.1"
   @description "A Google Cloud Pub/Sub connector for Broadway"
   @repo_url "https://github.com/dashbitco/broadway_cloud_pub_sub"
 
