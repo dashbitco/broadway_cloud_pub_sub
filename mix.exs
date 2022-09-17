aed4023331934df6b470c2d016b39724c336dd43
Michael Crumm <mike@crumm.net>
2022-09-16 19:13:04 -0700

    Loosen finch and nimble_options deps requirements

--- mix.exs
+++ mix.exs
@@ -33,9 +33,9 @@ defmodule BroadwayCloudPubSub.MixProject do
   defp deps do
     [
       {:broadway, "~> 1.0"},
-      {:finch, "~> 0.9.0"},
+      {:finch, "~> 0.9"},
       {:jason, "~> 1.0"},
-      {:nimble_options, "~> 0.3.7"},
+      {:nimble_options, "~> 0.3.0 or ~> 0.4.0"},
       {:telemetry, "~> 0.4.3 or ~> 1.0"},
       {:goth, "~> 1.0", optional: true},
       {:ex_doc, "~> 0.23", only: :docs},
