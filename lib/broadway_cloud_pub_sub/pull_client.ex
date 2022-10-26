0e6ecb670ad4e2d12248ccfc64fe5b77be64d87b
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2022-10-26 11:56:20 +0100

    Adds telemetry
    Adds span style events for pull request and ack, including tests and documentation.
    
    Happy to take suggestions on naming. Potentially the existing demand is not something we should emit here? Added it cause it'd find it personally useful but happy to remove if it doesn't make sense.
    
    It was a bit hard to do this because the config passed to ack doesn't come from the same place at all as the config passed to receive_messages, the first being just validated producer opts put in persistent term and not as you'd expect, the ack opts validated in the configure callback). And the second being the full config passed to the producer, including the broadway config.

--- lib/broadway_cloud_pub_sub/pull_client.ex
+++ lib/broadway_cloud_pub_sub/pull_client.ex
@@ -36,10 +36,23 @@ defmodule BroadwayCloudPubSub.PullClient do
   def receive_messages(demand, ack_builder, config) do
     max_messages = min(demand, config.max_number_of_messages)
 
-    config
-    |> execute(:pull, %{"maxMessages" => max_messages})
-    |> handle_response(:receive_messages)
-    |> wrap_received_messages(ack_builder)
+    :telemetry.span(
+      [:broadway_cloud_pub_sub, :pull_client, :pull_request],
+      %{
+        requested_messages: max_messages,
+        total_demand: demand,
+        topology_name: config.broadway[:name]
+      },
+      fn ->
+        result =
+          config
+          |> execute(:pull, %{"maxMessages" => max_messages})
+          |> handle_response(:receive_messages)
+          |> wrap_received_messages(ack_builder)
+
+        {result, %{topology_name: config.broadway[:name]}}
+      end
+    )
   end
 
   @impl Client
@@ -56,9 +69,18 @@ defmodule BroadwayCloudPubSub.PullClient do
 
   @impl Client
   def acknowledge(ack_ids, config) do
-    config
-    |> execute(:acknowledge, %{"ackIds" => ack_ids})
-    |> handle_response(:acknowledge)
+    :telemetry.span(
+      [:broadway_cloud_pub_sub, :pull_client, :ack],
+      %{topology_name: config.topology_name},
+      fn ->
+        result =
+          config
+          |> execute(:acknowledge, %{"ackIds" => ack_ids})
+          |> handle_response(:acknowledge)
+
+        {result, %{topology_name: config.topology_name}}
+      end
+    )
   end
 
   defp handle_response({:ok, response}, :receive_messages) do
